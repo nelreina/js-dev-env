9c1b977a342008eafb883c0c67adb7172a894698
Nelreina <nelreina@gmail.com>
2016-11-27 09:47:44 -0400

    added Webpack basic config and screated a dev script in packagejson

--- build/src-server.js
+++ build/src-server.js
@@ -1,14 +1,22 @@
 import express from 'express';
 import open from 'open';
 import path from 'path';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+import webpackDevMiddleware from 'webpack-dev-middleware';
 
 const port = 8080;
-
 const app = express();
+const compiler = webpack(config);
+
+app.use(webpackDevMiddleware(compiler, {
+	noInfo: true,
+	publicPath: config.output.publicPath
+}));
 
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '../src/index.html'));
-})
+});
 
 app.listen(port, err => {
 	if (err) {
@@ -16,4 +24,4 @@ app.listen(port, err => {
 	} else {
 		open(`http://localhost:${port}`);
 	}
-})
+});
